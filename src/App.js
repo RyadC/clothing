09c9cf929a1ffcab5f934ade325c04cf1a23b96f
Ryad Chair <r.chair@hotmail.fr>
2022-12-17 23:09:46 +0100

    Create a Components folder & Create a CategoryItem component & Create a category-item scss file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,42 @@
 import './App.css';
 import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.component';
 
 const App = () => {
 
   const catgeories = [
     {
-     id: 1,
-     title: 'Chapeau', 
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.pnghttps://i.ibb.co/cvpntL1/hats.png"
     },
     {
-     id: 2,
-     title: 'Vestes', 
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-     id: 3,
-     title: 'Baskets', 
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-     id: 4,
-     title: 'Femmes', 
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-     id: 5,
-     title: 'Hommes', 
-    },
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    }
   ]
+  
 
   return (
     <div className='categories-container'>
       {catgeories.map((category) => (
-         <div className='category-container'>
-          {/* <img /> */}
-          <div className='category-body-container'>
-            <h2>{category.title}</h2>
-            <p>Découvrir</p>
-          </div>
-        </div>
+        <CategoryItem key={category.id} category={category} />
       ))}
  
     </div>
