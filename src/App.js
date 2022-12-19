9551ce6690b2e9bf6270d7d489942cb55c4e7d16
Ryad Chair <r.chair@hotmail.fr>
2022-12-19 14:31:45 +0100

    Finished lecon-10

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import { Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 import Home from './routes/home/home.routes-component';
 
+
+const Navigation = () => {
+
+  return (
+    <Fragment>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </Fragment>
+  )
+}
+
+const Shop = () => {
+  return <h1>Shop page</h1>
+}
+
+
 const App = () => {
-
-  const Shop = () => {
-
-    return <h1>Shop page</h1>
-  }
 
  return (
   <Routes>
-    <Route path='/' element={<Home />}>
+    <Route path='/' element={<Navigation />}>
+      <Route index={true} element={<Home />} />
       <Route path='shop' element={<Shop />} />
     </Route>
   </Routes>
