cdcd7529cbf785897eb0dab077b4b3371643a0a2
Ryad Chair <r.chair@hotmail.fr>
2022-12-19 16:21:36 +0100

    Finished lecon-13

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 import Home from './routes/home/home.routes-component';
 import Navigation from './routes/navigation/navigation.routes-component';
+import SignIn from './routes/sign-in/sign-in.routes-component';
 
 const Shop = () => {
   return <h1>Shop page</h1>
@@ -15,6 +16,7 @@ const App = () => {
     <Route path='/' element={<Navigation />}>
       <Route index={true} element={<Home />} />
       <Route path='shop' element={<Shop />} />
+      <Route path='sign-in' element={<SignIn />} />
     </Route>
   </Routes>
  )
--- src/routes/navigation/navigation.routes-component.jsx
+++ src/routes/navigation/navigation.routes-component.jsx
@@ -1,5 +1,5 @@
 import { Fragment } from "react"
-import { Outlet, Link, NavLink } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import './navigation.styles.scss';
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
@@ -14,6 +14,7 @@ const Navigation = () => {
 
         <ul className="nav-links-container">
           <Link className="nav-link" to='/shop'>SHOP</Link>
+          <Link className="nav-link" to='/sign-in'>SIGN IN</Link>
         </ul>
       </nav>
 
