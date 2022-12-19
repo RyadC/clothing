93b7749e61331f079b93d429b46f08caed2bb217
Ryad Chair <r.chair@hotmail.fr>
2022-12-19 11:18:02 +0100

    Finished lecon-8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './routes/home/home.routes-component';
 
 const App = () => {
 
-  return (
-    <Routes>
-      <Route path='/' element={<Home />}>
-      </Route>
-    </Routes>
-  )
+ return (
+  <Routes>
+    <Route path='/' element={<Home />} />
+  </Routes>
+ )
+
 }
 
 export default App;
